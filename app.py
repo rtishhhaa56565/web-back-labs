52b809d4eac67e2c205747beeff6051f56132406
Арина Арышева <arina.arysheva2005@mail.ru>
2025-09-06 11:36:47 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start():
-    return "<!doctype html>" \
-        "<html>" \
-            "<body>" \
-                "<h1>web-сервер на flask</h1>" \
-            "</body>" \
-        "</html>"""
+@app.route("/web")
+def web():
+    return """<!doctype html>
+<html>
+<body>
+<h1>web-сервер на flask</h1>
+<p><a href="/author">Перейти к информации об авторе</a></p>
+</body>
+</html>"""
+
+@app.route("/author")
+def author():
+    name = "Иванов Иван Иванович"
+    group = "ФБИ-80"
+    faculty = "ФБ"
+    
+    return """<!doctype html>
+<html>
+<body>
+<p>Студент: """ + name + """</p>
+<p>Группа: """ + group + """</p>
+<p>Факультет: """ + faculty + """</p>
+<p><a href="/web">Вернуться на главную</a></p>
+</body>
+</html>"""
