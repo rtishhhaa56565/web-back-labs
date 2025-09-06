991f7b1c9abb4203fa53ab0b344e390398ab3aa5
Арина Арышева <arina.arysheva2005@mail.ru>
2025-09-06 09:59:35 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def start():
-    return 'web-сервер на flask'
+    return "<!doctype html>" \
+        "<html>" \
+            "<body>" \
+                "<h1>web-сервер на flask</h1>" \
+            "</body>" \
+    "</html>"
