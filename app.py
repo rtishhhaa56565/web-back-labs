d530f46b8811e872cdf16a0b49ca7c5c8052f4d2
Арина <arina.arysheva2005@mail.ru>
2025-09-19 14:16:51 +0700

    Картинка image.jpg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, url_for
+
 app = Flask(__name__)
 
 @app.route("/web")
@@ -8,6 +9,7 @@ def web():
 <body>
 <h1>web-сервер на flask</h1>
 <p><a href="/author">Перейти к информации об авторе</a></p>
+<p><a href="/lab1/image">Посмотреть картинку</a></p>
 </body>
 </html>"""
 
@@ -26,3 +28,20 @@ def author():
 <p><a href="/web">Вернуться на главную</a></p>
 </body>
 </html>"""
+
+@app.route("/lab1/image")
+def image():
+    # Получаем путь к картинке с помощью url_for
+    image_path = url_for('static', filename='image.jpg')
+    
+    return '''<!doctype html>
+<html>
+<head>
+    <title>Картинка</title>
+</head>
+<body>
+    <h1>Моя картинка</h1>
+    <img src="''' + image_path + '''" alt="Мое изображение" width="500">
+    <p><a href="/web">Вернуться на главную</a></p>
+</body>
+</html>'''
