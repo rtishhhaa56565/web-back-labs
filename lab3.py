7fcbd8a555f0f96fac85c78b7ad64cfd45c72075
Арина <arina.arysheva2005@mail.ru>
2025-10-17 17:47:05 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,16 +1,24 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, make_response, redirect
 
 lab3 = Blueprint('lab3', __name__)
 
-@lab3.route('/lab3/')
+@lab3.route('/')
 def lab():
     name = request.cookies.get('name')
-    return render_template('lab3/lab3.html', name=name)  # Рендерим шаблон вместо пустой строки
+    return render_template('lab3/lab3.html', name=name)
 
 @lab3.route('/cookie')
 def cookie():
-    resp = make_response('установка cookie', 200)
-    resp.set_cookie('name','Alex')
-    resp.set_cookie('age','20')
-    resp.set_cookie('color','magenta')
+    resp = make_response('установка cookie')
+    resp.set_cookie('name', 'Alex')
+    resp.set_cookie('age', '20')
+    resp.set_cookie('color', 'magenta')
+    return resp
+
+@lab3.route('/clear_cookies')
+def clear_cookies():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', '', expires=0)
+    resp.set_cookie('age', '', expires=0)
+    resp.set_cookie('color', '', expires=0)
     return resp
