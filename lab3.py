49d090cd3fe52d4700e0d063c50e5fba6442c40d
Арина <arina.arysheva2005@mail.ru>
2025-10-17 18:59:46 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, request, make_response, redirect
 
+# Сначала создаем Blueprint
 lab3 = Blueprint('lab3', __name__)
 
+# Затем используем декораторы
 @lab3.route('/')
 def lab():
     name = request.cookies.get('name')
@@ -25,13 +27,98 @@ def clear_cookies():
 
 @lab3.route('/form1', methods=['GET', 'POST'])
 def form1():
+    errors = {}
+    user = ''
+    age = ''
+    gender = ''
+
     if request.method == 'POST':
-        user = request.form.get('user', '')
+        user = request.form.get('user', '').strip()
         age = request.form.get('age', '')
         gender = request.form.get('gender', '')
-    else:
+
+        # Проверка имени пользователя
+        if not user:
+            errors['user'] = 'Заполните поле имени'
+        elif len(user) < 2:
+            errors['user'] = 'Имя должно содержать минимум 2 символа'
+        elif len(user) > 50:
+            errors['user'] = 'Имя не должно превышать 50 символов'
+
+        # Проверка возраста
+        if age:
+            try:
+                age_int = int(age)
+                if age_int < 1 or age_int > 120:
+                    errors['age'] = 'Возраст должен быть от 1 до 120 лет'
+            except ValueError:
+                errors['age'] = 'Возраст должен быть числом'
+
+    else:  # GET запрос
         user = request.args.get('user', '')
         age = request.args.get('age', '')
         gender = request.args.get('gender', '')
+
+    return render_template('lab3/form1.html', 
+                         user=user, age=age, gender=gender, 
+                         errors=errors)
+
+# НОВЫЕ ОБРАБОТЧИКИ ДЛЯ ЗАКАЗА НАПИТКА
+@lab3.route('/order', methods=['GET', 'POST'])
+def order():
+    if request.method == 'POST':
+        drink = request.form.get('drink')
+        sugar = 'sugar' in request.form
+        milk = 'milk' in request.form
+        
+        price = 0
+        if drink == 'coffee':
+            price = 200
+        elif drink in ['black-tea', 'green-tea']:
+            price = 150
+            
+        if sugar:
+            price += 10
+        if milk:
+            price += 20
+            
+        return render_template('lab3/pay.html', 
+                             drink=drink, sugar=sugar, milk=milk, 
+                             price=price)
     
-    return render_template('lab3/form1.html', user=user, age=age, gender=gender)
+    return render_template('lab3/order.html')
+
+@lab3.route('/pay', methods=['GET', 'POST'])
+def pay():
+    if request.method == 'POST':
+        drink = request.form.get('drink')
+        sugar = 'sugar' in request.form
+        milk = 'milk' in request.form
+        
+        price = 0
+        if drink == 'coffee':
+            price = 200
+        elif drink in ['black-tea', 'green-tea']:
+            price = 150
+            
+        if sugar:
+            price += 10
+        if milk:
+            price += 20
+            
+        return render_template('lab3/pay.html', 
+                             drink=drink, sugar=sugar, milk=milk, 
+                             price=price)
+    
+    return render_template('lab3/pay.html')
+
+@lab3.route('/success', methods=['POST'])
+def success():
+    drink = request.form.get('drink')
+    sugar = 'sugar' in request.form
+    milk = 'milk' in request.form
+    price = request.form.get('price')
+    
+    return render_template('lab3/success.html',
+                         drink=drink, sugar=sugar, milk=milk,
+                         price=price)
