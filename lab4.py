53b0d25547db6e07e2008334052a4e4921f1f077
Арина <arina.arysheva2005@mail.ru>
2025-10-31 17:57:03 +0700

    Деление

--- lab4.py
+++ lab4.py
@@ -2,6 +2,15 @@ from flask import Blueprint, render_template, request
 
 lab4 = Blueprint('lab4', __name__)
 
-@lab4.route('/lab4/')
+@lab4.route('/')
 def lab():
     return render_template('lab4/lab4.html')
+
+@lab4.route('/div', methods=['GET', 'POST'])
+def div():
+    if request.method == 'POST':
+        x1 = request.form.get('x1')
+        x2 = request.form.get('x2')
+        # Здесь будет обработка деления
+        return f"Результат: {x1} / {x2}"
+    return render_template('lab4/div-form.html')  
